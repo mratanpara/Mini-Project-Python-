164fec7daf0d53f8bef1c277d136283aaaf285e9
Mohit <mohitratanpara2001@gmail.com>
2020-08-22 14:35:23 +0530

    created and connected to a database named emp.db and emp table is created with appropriate columns..

--- connect.py
+++ connect.py
@@ -2,7 +2,16 @@ import sqlite3
 class myconnect:
       
       def __init__(self):
-            #4
+            self.con = sqlite3.connect("emp.db")
+            self.con.execute(''' create table if not exists emp(
+                              id integer primary  key AUTOINCREMENT,
+                              name text,
+                              email text,
+                              mobile_no text,
+                              type text,
+                              experience integer,
+                              salary text
+                        ) ''')
             #5      
                   
       def savetodb(self,ename,eemail,emob,etype,eexp,esalary):
